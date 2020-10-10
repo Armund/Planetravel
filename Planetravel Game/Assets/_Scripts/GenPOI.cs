44adbf5f4fd5fc0b3bb08029c012f5d91189b211
AesenZero <aesenzero@ukr.net>
2020-10-10 20:19:16 +0300

    Базовая реализация Лаборатории и Генератора. Добавление игрового менеджера
    Тестовая сцена, где присутстуют прототипы точек интереса (Лаборатория, Генератор). Генератор на старте рандомно определяет время до того как отключится. В это время Лаборатория заполняет контейнер. Если Генератор успеет выключится, то Лаборатория перестанет работать. Отключение электричества реализовано через Игрового Менеджера.

---
+++ Planetravel Game/Assets/_Scripts/GenPOI.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Клас Генератора Электричества
+public class GenPOI : POI_Object
+{
+    private MeshRenderer MR;
+    public Material genOnMat;
+    public Material genOffMat;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        MR = GetComponent<MeshRenderer>();
+        MR.material = genOnMat;
+        poiName = "Generator";
+        isElectrical = true;
+        status = PoiStatus.Active;
+        timeBeforeNextEvent = Random.Range(minTimeBeforeEvent, maxTImeBeforeEvent);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (status)
+        {
+            case PoiStatus.Active:
+                {
+                    Tick();
+                    MR.material = genOnMat;
+                }
+                break;
+            case PoiStatus.Broken:
+                {
+                    Destroy(gameObject);
+                }
+                break;
+            case PoiStatus.Disabled:
+                {
+                    GM.inst.Electricity = false;
+                    MR.material = genOffMat;
+                }
+                break;
+            case PoiStatus.Event:
+                {
+                    status = PoiStatus.Disabled;
+                }
+                break;
+        }
+    }
+
+
+
+
+}
---
+++ Planetravel Game/Assets/_Scripts/LabPOI.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LabPOI : POI_Object
+{
+    public float containerСapacity = 500;
+    public float curCapacity = 0;
+    private float limitCap = 100;
+    public float occupancy = 2;
+    public Vector3 occupScale;
+    public GameObject container;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        poiName = "Lab";
+        isElectrical = true;
+        status = PoiStatus.Active;
+        container = transform.Find("ConPivot").gameObject;
+        occupScale = new Vector3(0,((1 - 0.01f) / (containerСapacity / occupancy)),0);
+        limitCap = containerСapacity / 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        switch(status)
+        {
+            case PoiStatus.Active:
+                {
+                    fillingUP();
+                }
+                break;
+            case PoiStatus.Broken:
+                {
+                    Destroy(container);
+                }
+                break;
+            case PoiStatus.Disabled:
+                {
+
+                }
+                break;
+            case PoiStatus.Event:
+                {
+
+                }
+                break;
+        }
+
+    }
+
+    //Заполнения контейнера с топливом. Будучи заполненым, контейнер какое-то время еще будет заполнятся, пока не сломается.
+    public void fillingUP()
+    {
+
+        if (curCapacity > containerСapacity + limitCap)
+        {
+            curCapacity = 0;
+            status = PoiStatus.Broken;
+        }else curCapacity += occupancy * Time.deltaTime;
+
+        if (curCapacity < containerСapacity)
+        {           
+            container.transform.localScale += occupScale *Time.deltaTime;
+        }
+    }
+
+  
+}
