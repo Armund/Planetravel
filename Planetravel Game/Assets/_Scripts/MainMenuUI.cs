4a196e1f286fcf5578d02709b100c9361d92847f
AesenZero <aesenzero@ukr.net>
2020-10-16 19:25:20 +0300

    minor

--- Planetravel Game/Assets/_Scripts/MainMenuUI.cs
+++ Planetravel Game/Assets/_Scripts/MainMenuUI.cs
@@ -2,15 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MainMenuUI : MonoBehaviour
 {
+    public Button StartButton;
+    public Button ExitButton;
+    
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    public void StartTheGame()
+    {
+        SceneManager.LoadScene("SessionScene", LoadSceneMode.Single);
+    }
+
+    public void ExitTheGame()
+    {
+        Application.Quit();
+    }
     // Update is called once per frame
     void Update()
     {
