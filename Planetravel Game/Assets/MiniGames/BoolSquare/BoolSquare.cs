79a342fe7594edf4268ea01655a3e0a4c15a7b06
Armund <erm_alex@list.ru>
2020-10-20 08:34:11 +0300

    Minigame BoolSquare

---
+++ Planetravel Game/Assets/MiniGames/BoolSquare/BoolSquare.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BoolSquare : MiniGame
+{
+	public Canvas canvas;
+	public Text winText;
+
+	public Image[] lightImages = new Image[6];
+	public Sprite redLight;
+	public Sprite greenLight;
+
+	//игровые значения
+	bool[] lightValues = new bool[6];
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		Init();
+    }
+
+	public override void Init() {
+		if (!isStarted) {
+			canvas.gameObject.SetActive(true);
+
+			winText.text = "";
+
+			for (int i = 0; i < 6; i++) {
+				lightValues[i] = true;
+				lightImages[i].sprite = greenLight;
+			}
+			ButtonPressed(0);
+
+			//генерируем сетап
+			while (lightValues[3] & lightValues[4] & lightValues[5]) {
+				for (int i = 1; i < 4; i++) {
+					int rand = Random.Range(0, 3);
+					if (rand > 0) {
+						ButtonPressed(i);
+					}
+				}
+			}
+
+			isStarted = true;
+		}
+	}
+
+	override public void Close() {
+		if (isStarted) {
+			canvas.gameObject.SetActive(false);
+			poi.EventLosing();
+			isStarted = false;
+		}
+	}
+
+	public void ButtonPressed(int but) {
+		switch (but) {
+			case 0:
+				SwitchLight(0);
+				SwitchLight(1);
+				SwitchLight(2);
+				break;
+			case 1:
+				SwitchLight(0);
+				SwitchLight(3);
+				break;
+			case 2:
+				SwitchLight(1);
+				SwitchLight(4);
+				break;
+			case 3:
+				SwitchLight(2);
+				SwitchLight(5);
+				break;
+		}
+		if (IsGameOver()) {
+			winText.text = "WIN";
+			winText.color = Color.green;
+		}
+	}
+
+	void SwitchLight(int val) {
+		lightValues[val] = !lightValues[val];
+		if (lightValues[val]) {
+			lightImages[val].sprite = greenLight;
+		} else {
+			lightImages[val].sprite = redLight;
+		}
+	}
+
+	bool IsGameOver() {
+		foreach (bool val in lightValues) {
+			if (!val) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
---
+++ Planetravel Game/Assets/MiniGames/ElectroLock/Lock.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Lock : MiniGame
+{
+	public Canvas canvas;
+	public Text codeText;
+	public Text winText;
+	public Text currentCodeText;
+
+	List<int> goal = new List<int>();
+	List<int> currentCode = new List<int>();
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		//Init();
+    }
+
+	public override void Init() {
+		if (!isStarted) {
+			canvas.gameObject.SetActive(true);
+
+			codeText.text = "";
+			winText.text = "LOCKED";
+			winText.color = Color.red;
+			currentCodeText.text = "";
+			for (int i = 0; i < 4; i++) {
+				goal.Add(Random.Range(0, 10));
+				codeText.text += goal[i].ToString();
+			}
+
+			isStarted = true;
+		}
+	}
+
+	override public void Close() {
+		if (isStarted) {
+			canvas.gameObject.SetActive(false);
+			poi.EventLosing();
+			isStarted = false;
+		}
+	}
+
+	public void ButtonPressed(int but) {
+		currentCode.Add(but);
+		currentCodeText.text += but.ToString();
+		if (IsGameOver()) {
+			winText.text = "UNLOCKED";
+			winText.color = Color.green;
+		} else if (currentCode.Count == 4) {
+			currentCodeText.text = "";
+			currentCode.Clear();
+		}
+	}
+
+	public bool IsGameOver() {
+		for (int i = 0; i < 4; i++) {
+			if (currentCode.Count < 4 || goal[i] != currentCode[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
